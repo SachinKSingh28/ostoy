a70d080efb4bcafab6ab985f9f766ded41495f6f
Sachin Kumar Singh <sachin.kumar.singh@accenture.com>
2022-02-10 18:27:54 +0000

    chore(*): removes comments

--- microservice/app.js
+++ microservice/app.js
@@ -5,8 +5,8 @@ app.set('port', process.env.PORT || 8080);
 let pod = process.env.HOSTNAME || 'unknown-pod';
 
 app.get('/', function(request, response) {
-  //let randomColor = getRandomColor(); // <-- comment this
-  let randomColor = getRandomGrayScaleColor(); // <-- uncomment this
+  //let randomColor = getRandomColor();
+  let randomColor = getRandomGrayScaleColor();
 
   response.writeHead(200, {'Content-Type': 'application/json'});
   response.end(JSON.stringify({
